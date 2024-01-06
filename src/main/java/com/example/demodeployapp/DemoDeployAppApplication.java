30cffbec20d789a5a42f255ab5dc91705b8773c7
Hemma <hemma1108@gmail.com>
2024-01-06 12:19:09 +0800

    demoheroku deployment app

---
+++ src/main/java/com/example/demodeployapp/DemoDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demodeployapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeployAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeployAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demodeployapp/DemoDeployAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeployapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeployAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
